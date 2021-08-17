271670af96cddddf9c1dd1f7892b256f244f11b3
Nikola Yanakiev <nikola.yanakiev0@gmail.com>
2021-08-17 15:41:01 +0300

    Add milliseconds param to RakServer::AddToBanList

--- Include/raknet/RakServerInterface.h
+++ Include/raknet/RakServerInterface.h
@@ -375,7 +375,7 @@ namespace RakNet
 
 		/// Bans an IP from connecting.  Banned IPs persist between connections.
 		/// param[in] IP Dotted IP address. Can use * as a wildcard, such as 128.0.0.* will banAll IP addresses starting with 128.0.0
-		virtual void AddToBanList( const char *IP )=0;
+		virtual void AddToBanList( const char *IP, RakNetTime milliseconds=0 )=0;
 
 		/// Allows a previously banned IP to connect. 
 		/// param[in] Dotted IP address. Can use * as a wildcard, such as 128.0.0.* will banAll IP addresses starting with 128.0.0
--- Source/rakserver.cpp
+++ Source/rakserver.cpp
@@ -526,9 +526,9 @@ PlayerID RakServer::GetPlayerIDFromIndex( int index )
 	return RakPeer::GetPlayerIDFromIndex( index );
 }
 
-void RakServer::AddToBanList( const char *IP )
+void RakServer::AddToBanList( const char *IP, RakNetTime milliseconds )
 {
-	RakPeer::AddToBanList( IP );
+	RakPeer::AddToBanList( IP, milliseconds );
 }
 
 void RakServer::RemoveFromBanList( const char *IP )
